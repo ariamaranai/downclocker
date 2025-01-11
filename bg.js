7379c3297424be1cfd9247ce0816069a9797efe8
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-11 10:29:32 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,72 +1,53 @@
-(chrome => {
-  let debugGroupId;
-  let rate;
-  let hasBeforeNavigate = 0;
-  let attach = target => {
-    chrome.action.setBadgeText({ tabId: target.tabId, text: "x" + rate }),
+{
+  let dbgTabId = 0;
+  let badgeText = 0;
+  let dbgAttach = (tabId, rate) => {
+    let target = { tabId };
     chrome.debugger.attach(target, "1.3"),
     chrome.debugger.sendCommand(target, "Target.setAutoAttach", {
       autoAttach: !0,
-      waitForDebuggerOnStart: !1,
+      waitForDebuggerOnStart: !1
     }),
     chrome.debugger.sendCommand(target, "Emulation.setCPUThrottlingRate", {
       rate: +rate
-    });
-    hasBeforeNavigate || (
-      chrome.webNavigation.onBeforeNavigate.addListener(tracker),
-      hasBeforeNavigate = 1
+    }),
+    chrome.tabs.onActivated.addListener(tabActivatedHandler);
+    chrome.action.setBadgeText({ text: badgeText = "x" + rate })
+  }
+  let tabActivatedHandler = activeInfo =>
+    chrome.action.setBadgeText({ text: activeInfo.tabId == dbgTabId ? badgeText : "" })
+  let beforeNavigateHandler = details =>
+    details.url[0] != "c" && details.tabId == dbgTabId && (
+      chrome.webNavigation.onBeforeNavigate.removeListener(beforeNavigateHandler),
+      dbgAttach(dbgTabId = details.tabId, badgeText.slice(1))
     );
-  }
-  let tracker = async tab => {
-    let tabId = tab.id;
-    if (tab.url[0] != "c" && !(await chrome.debugger.getTargets()).find(v => v.tabId == tabId).attached)
-      attach({ tabId });
-  }
-  chrome.action.setBadgeTextColor({ color: "#fff" });
-  
-  chrome.debugger.onDetach.addListener(() => {
-    debugGroupId = 0;
-    chrome.webNavigation.onBeforeNavigate.removeListener(tracker);
-  });
-  chrome.tabGroups.onRemoved.addListener(g => {
-    if (debugGroupId) {
-      if (g.id == debugGroupId) {
-        debugGroupId = 0;
-        chrome.webNavigation.onBeforeNavigate.removeListener(tracker);
-      }
-    }
-  });
+
+  chrome.debugger.onDetach.addListener(() => (
+    chrome.tabs.onUpdated.removeListener(beforeNaivateHandler),
+    chrome.action.setBadgeText({ text: "" })
+  ));
   chrome.runtime.onMessage.addListener(async m => {
     let tab = m[0];
-    rate = m[1];
+    let rate = m[1];
     let tabId = tab.id;
-    let groupId = tab.groupId;
     let target = { tabId };
     if ((await chrome.debugger.getTargets()).find(v => v.tabId == tabId)?.attached)
-      if (rate != "1" && groupId == debugGroupId) (
-        chrome.action.setBadgeText({ tabId, text: "x" + rate }),
-        chrome.debugger.sendCommand(target, "Emulation.setCPUThrottlingRate", {
-          rate: +rate
-        })
-      )
-      else (
-        chrome.action.setBadgeText({ tabId, text: "" }),
-        chrome.debugger.detach(target),
-        chrome.tabs.ungroup(tabId)
-      );
+      rate != "1"
+        ? (
+          chrome.debugger.sendCommand(target, "Emulation.setCPUThrottlingRate", {
+            rate: +rate
+          }),
+          chrome.action.setBadgeText({ text: badgeText = "x" + rate })
+        )
+        : (
+          chrome.tabs.onUpdated.removeListener(beforeNavigateHandler),
+          chrome.action.setBadgeText({ text: "" })
+        )
     else if (rate != "1") {
-      let tabIds = [tabId];
-      if (debugGroupId) {
-        if (groupId != debugGroupId) {
-          tabIds = (await chrome.tabs.query({ groupId: debugGroupId })).map(v => v.id).concat(tabIds);
-          chrome.tabs.group({ groupId: debugGroupId, tabIds });
-        }
-      } else {
-        debugGroupId = await chrome.tabs.group({tabIds});
-        await chrome.tabGroups.update(debugGroupId, { title: "Downclocker" });
-      }
-      if (tab.url[0] != "c")
-        attach(target);
-      }
+      if (tab.url[0] == "c")
+        chrome.webNavigation.onBeforeNavigate.addListener(beforeNavigateHandler);
+    dbgAttach(dbgTabId = tabId, rate);
+    }
   });
-})(chrome);
+}
+chrome.action.setBadgeTextColor({ color: "#fff" });
--- popup.js
+++ popup.js
@@ -2,6 +2,6 @@ oninput = async e => chrome.runtime.sendMessage([
   (await chrome.tabs.query({ active: !0, currentWindow: !0 }))[0],
   e.target.value
 ]);
-chrome.tabs.query({ active: !0, currentWindow: !0 }).then(async tab =>
-  document.body.firstChild.value = (await chrome.action.getBadgeText({ tabId: tab[0].id })).slice(1) || "1"
+chrome.tabs.query({ active: !0, currentWindow: !0 }).then(async () =>
+  document.body.firstChild.value = (await chrome.action.getBadgeText({})).slice(1) || "1"
 );
