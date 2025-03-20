ac30eab608867d457b30e49204aabde8e1e865b4
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-20 17:38:38 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,13 +1,12 @@
-oninput = e => {
-  chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
-    let tab = tabs[0];
-    let rate = +e.target.value;
-    chrome.runtime.sendMessage(rate > 1 && (
-      tab.url[0] == "c" && chrome.tabs.update({ url: "about:blank" }),
-      [rate, tab.id]
-    ));
-  });
-}
+oninput = e => chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
+  let tab = tabs[0];
+  let rate = +e.target.value;
+  chrome.runtime.sendMessage(rate > 1 && (
+    tab.url[0] == "c" && chrome.tabs.update({ url: "about:blank" }),
+    [rate, tab.id]
+    )
+  );
+});
 chrome.tabs.query({ active: !0, currentWindow: !0 }, () =>
   chrome.action.getBadgeText({}, result =>
     document.body.firstChild.value = result.slice(1) || "1"
