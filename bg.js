7fe31d18911c186f06beb4b00a1aeaf41e246137
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-31 03:36:39 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -12,19 +12,14 @@
       let rate = m[0];
       let tabId = m[1];
       let target = { tabId };
-      tabId == dbgTabId
-        ? (
-          chrome.debugger.sendCommand(target, "Emulation.setCPUThrottlingRate", { rate }),
-          chrome.action.setBadgeText({ text: badgeText = "x" + rate })
-        )
-        : (
-          chrome.debugger.attach(target, "1.3"),
-          chrome.debugger.sendCommand(target, "Target.setAutoAttach", { autoAttach: !0, waitForDebuggerOnStart: !1 }),
-          chrome.debugger.sendCommand(target, "Emulation.setCPUThrottlingRate", { rate }),
-          chrome.action.setBadgeText({ text: badgeText = "x" + rate }),
-          dbgTabId = tabId,
-          chrome.tabs.onActivated.addListener(tabActivatedHandler)
-        );
+      tabId != dbgTabId && (
+        chrome.debugger.attach(target, "1.3"),
+        chrome.debugger.sendCommand(target, "Target.setAutoAttach", { autoAttach: !0, waitForDebuggerOnStart: !1 }),
+        dbgTabId = tabId,
+        chrome.tabs.onActivated.addListener(onTabActivated)
+      );
+      chrome.debugger.sendCommand(target, "Emulation.setCPUThrottlingRate", { rate }),
+      chrome.action.setBadgeText({ text: badgeText = "x" + rate })
     } else
       chrome.debugger.onDetach.dispatch();
   });
